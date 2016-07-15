3dbf28b4896ab0de1d69d016daf2a9a6d42fc214
artist <artist@studyGit.com>
2016-07-15 23:26:56 +0900

    Initial Commit

---
+++ evvTableGroupObjc101/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  evvTableGroupObjc101
+//
+//  Created by artist on 7/15/16.
+//  Copyright © 2016 Ever8greener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ evvTableGroupObjc101/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  evvTableGroupObjc101
+//
+//  Created by artist on 7/15/16.
+//  Copyright © 2016 Ever8greener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
